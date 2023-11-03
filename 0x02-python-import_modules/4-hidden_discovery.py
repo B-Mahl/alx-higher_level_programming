75f111d6f18463015f081f2bf25ad3e02f00ca6e
B-Mahl <banelemahlangu@gmail.com>
2023-11-03 03:58:07 +0200

    dir()

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 from hidden_4 import *
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
+    names = dir()
     for name in names:
         if not name.startswith("_"):
             print(name)
