c31eea8914f20b1b44bbce4e59d29cfaf94ac571
B-Mahl <banelemahlangu@gmail.com>
2023-11-03 02:18:25 +0200

    fixed a typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     num_args = len(args) - 1
 
     if num_args == 0:
-        print('0 Arguments.')
+        print('0 arguments.')
     elif num_args == 1:
-        print('1 Argument:')
+        print('1 argument:')
         print('1: ' + args[1])
     else:
-        print('{} Arguments:'.format(num_args))
+        print('{} arguments:'.format(num_args))
         for i in range(1, len(args)):
             print('{}: {}'.format(i, args[i]))
