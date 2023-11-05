71d21f526c1f852641a4c88ad799d39ea24fd9fe
B-Mahl <banelemahlangu@gmail.com>
2023-11-05 06:08:20 +0200

    changed comparison operators

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if len(my_list) <= idx < 0:
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
         return my_list
     else:
-        my_list[idx] = element
         return my_list
