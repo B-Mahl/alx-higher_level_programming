bce50b1426f6316309ab6ea9fdb3e1ba9e2170fd
B-Mahl <banelemahlangu@gmail.com>
2023-11-06 21:14:17 +0200

    fixed formatting

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
             end_chr = ' ' if element + 1 != len(row) else ''
-            print('{:d}'.format(row[element]), end_chr)
+            print('{:d}'.format(row[element]), end=end_chr)
         print()
