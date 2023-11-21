3e93ace53897404b13c6f8cdc47d4d104588e160
B-Mahl <banelemahlangu@gmail.com>
2023-11-22 00:37:18 +0200

    Area of a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+# Banele Mahlangu <banelemahlangu@gmail.com>
+"""Define a class Sqaure"""
+
+class Square:
+    """Represnt a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new sqaure.
+        """
+        self._size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 2-sqaure.py
+# Banele Mahlangu <banelemahlangu@gmail.com>
+"""Define a class Square."""
+
+class Square:
+    """Represnt a new Square.
+
+    Args:
+        size (int): The size of the new sqaure.
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    self.__size = size
