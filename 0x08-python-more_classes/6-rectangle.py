ad9ea6386c1dea49692a56c5eb4bb217d575763c
B-Mahl <banelemahlangu@gmail.com>
2024-02-09 14:27:13 +0200

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -73,5 +73,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
-        number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
