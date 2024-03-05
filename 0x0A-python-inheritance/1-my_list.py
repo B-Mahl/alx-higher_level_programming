ac3cfe16de075fd2238685b54e53c171fa93f948
B-Mahl <banelemahlangu@gmail.com>
2024-03-05 23:54:40 +0200

    Exact same object

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Defines a subclass Mylist inherited from superclass list"""
 
 
-class Mylist(list):
+class MyList(list):
     """Subclass inherited from superclass list"""
 
     def print_sorted(self):
